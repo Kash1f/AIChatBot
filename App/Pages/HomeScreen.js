cb0c2c649abdc5b5798908d7bc2190a56d7e176e
Kashif <ahmed.kashif1001@gmail.com>
2024-04-02 23:51:50 +0500

    homescreen functionality

--- App/Pages/HomeScreen.js
+++ App/Pages/HomeScreen.js
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import ChatFaceData from './Services/ChatFaceData'
 
 const HomeScreen = () => {
+
+    const [chatFaceData, setChatFaceData] = useState()
+    const [selectedchatFaceData, setSelectedChatFaceData] = useState()
+
+    useEffect(()=>{
+        setChatFaceData(ChatFaceData)
+        setSelectedChatFaceData(ChatFaceData[0])
+    },[])
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView style={{marginTop:40}}>
+      <Text style={[{color:selectedchatFaceData.primary}]}>HomeScreen</Text>
+      <Text> I am {selectedchatFaceData.name}</Text>
+    </SafeAreaView>
   )
 }
 
 export default HomeScreen
+
+//setSelectedChatFaceData will help us to hold value for the selected
+//chatface, so whatever chatface is selected, it will hold value for that one
+
+//16:00
