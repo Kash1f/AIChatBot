5eef1fbb267fc68bb6e626fcae99b7978f54f91a
Kashif <ahmed.kashif1001@gmail.com>
2024-04-04 14:06:41 +0500

    added navigation, stack navigator

---
+++ App/Navigation/HomeScreenNavigation.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import ChatScreen from '../Pages/ChatScreen';
+import HomeScreen from '../Pages/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreenNavigation = () => {
+  return (
+    <Stack.Navigator>
+        <Stack.Screen name='chat' component={HomeScreen}></Stack.Screen>
+        <Stack.Screen name='chat' component={ChatScreen}></Stack.Screen>
+
+
+    </Stack.Navigator>
+  )
+}
+
+export default HomeScreenNavigation
